eaa69850159f2aac34c08ee86620e079d04f2f1c
viesal <gviesal@gmail.com>
2017-05-16 10:36:36 +0500

    пытаюсь реализовать таргетирование

--- manage.py
+++ manage.py
@@ -5,65 +5,67 @@ import cv2.cv as cv
 from datetime import datetime
 import time
 
-# #img = cv2.imread("1.jpeg")
-cap1 = cv.CaptureFromCAM(-1)
+img1 = cv.LoadImage("1.jpeg")
+img2 = cv.LoadImage("2.jpeg")
+# cap1 = cv.CaptureFromCAM(-1)
 cv.NamedWindow("capture", cv.CV_WINDOW_AUTOSIZE)
 #
-def CreateImg(capture, capture_name):
-    img = cv.QueryFrame(capture)
-    #cv.ShowImage(capture_name, img)
-    #cv.WaitKey(10)
-    path = u"img/cap{}.jpg".format(datetime.strftime(datetime.now(), "%S%f "))  # Уникальное имя для каждого кадра
-    cv.SaveImage(path, img)
-    return path
+# def CreateImg(capture, capture_name):
+#     img = cv.QueryFrame(capture)
+#     #cv.ShowImage(capture_name, img)
+#     #cv.WaitKey(10)
+#     path = u"img/cap{}.jpg".format(datetime.strftime(datetime.now(), "%S%f "))  # Уникальное имя для каждого кадра
+#     cv.SaveImage(path, img)
+#     return path
 
-img = cv.LoadImage(CreateImg(cap1, 'capture'))
-g = cv.CreateImage(cv.GetSize(img), cv.IPL_DEPTH_8U, 1)
-b = cv.CreateImage(cv.GetSize(img), cv.IPL_DEPTH_8U, 1)
-diff = cv.CreateImage(cv.GetSize(img), cv.IPL_DEPTH_8U, 3)
-smoth = cv.CreateImage(cv.GetSize(img), cv.IPL_DEPTH_8U, 1)
-hsv = cv.CreateImage(cv.GetSize(img), cv.IPL_DEPTH_8U, 3)
-diff2 = cv.CreateImage(cv.GetSize(img), cv.IPL_DEPTH_8U, 3)
+g = cv.CreateImage(cv.GetSize(img1), cv.IPL_DEPTH_8U, 1)
+b = cv.CreateImage(cv.GetSize(img1), cv.IPL_DEPTH_8U, 1)
+diff = cv.CreateImage(cv.GetSize(img1), cv.IPL_DEPTH_8U, 3)
+smoth = cv.CreateImage(cv.GetSize(img1), cv.IPL_DEPTH_8U, 1)
+hsv = cv.CreateImage(cv.GetSize(img1), cv.IPL_DEPTH_8U, 3)
+diff2 = cv.CreateImage(cv.GetSize(img1), cv.IPL_DEPTH_8U, 3)
 
+# img1 = cv.QueryFrame(cap1)
+# time.sleep(5)
+# img2 = cv.QueryFrame(cap1)
 
-img1 = cv.QueryFrame(cap1)
-time.sleep(5)
-img2 = cv.QueryFrame(cap1)
+# cv.ShowImage('capture', img1)
+# cv.ShowImage('capture1', img2)
+# cv.WaitKey()
 
-cv.ShowImage('capture', img1)
-cv.ShowImage('capture1', img2)
-cv.WaitKey()
+# while 1:
+#
+#     cv.Zero(g)
+#     cv.Zero(b)
+#     cv.Zero(diff)
+#     cv.Zero(smoth)
+#     cv.Zero(hsv)
+#     cv.Zero(diff2)
+#     print time.clock()
 
-while 1:
+    # img1 = cv.LoadImage(CreateImg(cap1, 'capture'))
+    # cv.ShowImage('img1', img1)
+    # cv.WaitKey()
+    # img2 = cv.LoadImage(CreateImg(cap1, 'capture'))
+    # cv.ShowImage('img2', img2)
 
-    cv.Zero(g)
-    cv.Zero(b)
-    cv.Zero(diff)
-    cv.Zero(smoth)
-    cv.Zero(hsv)
-    cv.Zero(diff2)
-    print time.clock()
+cv.Sub(img1, img2, diff)
+path = u"img/sub{}.jpg".format(datetime.strftime(datetime.now(), "%S%f "))  # Уникальное имя для каждого кадра
+cv.SaveImage(path, diff)
+cv.CvtColor(diff, hsv, cv2.COLOR_BGR2HSV)
 
-    img1 = cv.LoadImage(CreateImg(cap1, 'capture'))
-    cv.ShowImage('img1', img1)
-    cv.WaitKey()
-    img2 = cv.LoadImage(CreateImg(cap1, 'capture'))
-    cv.ShowImage('img2', img2)
+lower_wite = cv.Scalar(50, 50, 50)
+uper_wite = cv.Scalar(255, 255, 255)
 
-    cv.Sub(img1, img2, diff)
-    cv.ShowImage('capture1', diff)
-    cv.CvtColor(diff, hsv, cv2.COLOR_BGR2HSV)
+frame_threshed = cv.CreateImage(cv.GetSize(hsv), 8, 1)
+cv.InRangeS(hsv, lower_wite, uper_wite, frame_threshed)
+path = u"img/mask{}.jpg".format(datetime.strftime(datetime.now(), "%S%f "))  # Уникальное имя для каждого кадра
+cv.SaveImage(path, frame_threshed)
+cv.Copy(img2, diff2, frame_threshed)
 
-    lower_wite = cv.Scalar(50, 50, 50)
-    uper_wite = cv.Scalar(255, 255, 255)
-
-    frame_threshed = cv.CreateImage(cv.GetSize(hsv), 8, 1)
-    cv.InRangeS(hsv, lower_wite, uper_wite, frame_threshed)
-    cv.Copy(img2, diff2, frame_threshed)
-
-    cv.ShowImage('capture', diff2)
-    path = u"img/fin{}.jpg".format(datetime.strftime(datetime.now(), "%S%f "))  # Уникальное имя для каждого кадра
-    cv.SaveImage(path, diff2)
+cv.ShowImage('capture', diff2)
+path = u"img/fin{}.jpg".format(datetime.strftime(datetime.now(), "%S%f "))  # Уникальное имя для каждого кадра
+cv.SaveImage(path, diff2)
 
 
 
--- motion.py
+++ motion.py
@@ -4,39 +4,73 @@
 import sys
 import os
 import argparse
-import datetime
+from datetime import datetime
 import imutils
 import time
 import cv2
 import numpy as np
+import threading
 
 
 camera = cv2.VideoCapture(0)
 back_frame = None
-
+def save_img(path, img):
+    cv2.imwrite(path, img)
 
 
 # loop over the frames of the video
 while True:
-    # time.sleep(0.5)
+    #print datetime.strftime(datetime.now(), "%SS ")
     (grabbed, frame) = camera.read()
     if not grabbed:
         break
-    frame = imutils.resize(frame, width=500)  # откусим от видеокадра 500 px
-    gray = frame.copy()
+    frame = imutils.resize(frame, width=500) # откусим от видеокадра 500 px
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # filtr = cv2.inRange(frame, np.array([0, 69, 255]), np.array([255, 255, 255]))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edged = cv2.Canny(gray, 100, 250)
+    edged1 = cv2.bitwise_and(frame, frame, mask=edged)
+
+    (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    coins = frame.copy()
+    cv2.drawContours(coins, cnts, -1, (0, 255, 0), 2)
+
+    available_frame = frame.copy()
     if back_frame is not None:
-        frameDelta = cv2.absdiff(gray, back_frame)
+        frameDelta = cv2.absdiff(available_frame, back_frame)
         cv2.cvtColor(frameDelta, cv2.COLOR_BGR2GRAY)
-        (B, G, R) = cv2.split(frameDelta)
-        (T, threshB) = cv2.threshold(B, 50, 255, cv2.THRESH_BINARY)
-        (T, threshG) = cv2.threshold(G, 50, 255, cv2.THRESH_BINARY)
-        (T, threshR) = cv2.threshold(R, 50, 255, cv2.THRESH_BINARY)
-        merge = cv2.merge([threshB, threshG, threshR])
-        merge = cv2.cvtColor(merge, cv2.COLOR_BGR2GRAY)
+
+        # (B, G, R) = cv2.split(frameDelta)
+        # (T, threshB) = cv2.threshold(B, 50, 255, cv2.THRESH_BINARY)
+        # (T, threshG) = cv2.threshold(G, 50, 255, cv2.THRESH_BINARY)
+        # (T, threshR) = cv2.threshold(R, 50, 255, cv2.THRESH_BINARY)
+        # merge = cv2.merge([threshB, threshG, threshR])
+
+        (T, threshAll) = cv2.threshold(frameDelta, 50, 255, cv2.THRESH_BINARY)
+        merge = cv2.cvtColor(threshAll, cv2.COLOR_BGR2GRAY)
+        # merge = cv2.cvtColor(merge, cv2.COLOR_BGR2GRAY)
         (T, thresh) = cv2.threshold(merge, 10, 255, cv2.THRESH_BINARY)
-        masked = cv2.bitwise_and(gray, gray, mask=thresh)
-        print masked.__
+        masked = cv2.bitwise_and(available_frame, available_frame, mask=thresh)
+
+        # Сохранение изображений при ненулевой маске
+        # print np.sum(masked)
+        # if np.sum(masked)>2550:
+        #     path = u"img/mask{}.jpg".format(datetime.strftime(datetime.now(), "%S%f "))  # Уникальное имя для каждого кадра
+        #     thr = threading.Thread(target=save_img, args=(path, masked))
+        #     thr.deamon = True
+        #     thr.start()
+
         cv2.imshow("masked", masked)
+        cv2.imshow("coins", coins)
+        cv2.imshow("edged", edged1)
+
+        # cv2.imshow("gray", gray)
+        # cv2.imshow("masked1", threshAll)
+        # for width in thresh:
+        #     for item in width:
+        #         if item > 10:
+        #             path = u"img/fin{}.jpg".format(datetime.strftime(datetime.now(), "%S%f "))  # Уникальное имя для каждого кадра
+        #             cv2.imwrite(path, masked)
 
     back_frame = frame.copy()
     key = cv2.waitKey(1) & 0xFF
